9632640ec4ec5de8a1c42b51ba702b98d4fcd582
Виталий Сапрыгин <saprygin@altasib.ru>
2022-12-05 12:20:03 +0700

    new Activity

--- app/src/main/java/com/vsaprygin/sms/MainActivity.kt
+++ app/src/main/java/com/vsaprygin/sms/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.vsaprygin.sms
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.vsaprygin.sms.databinding.ActivityFirstBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFirstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonSave.setOnClickListener {
+            val newSetting = binding.newValue.text.toString()
+            if (newSetting == binding.textSeting.text.toString()) {
+                Toast.makeText(this, "Изменений нет", Toast.LENGTH_SHORT).show()
+            }
+            else {Toast.makeText(this, binding.textSeting.text.toString(), Toast.LENGTH_SHORT).show()
+                binding.textSeting.text = newSetting
+            }
+        }
+        binding.buttonAdvanset.setOnClickListener {
+            val intent = Intent(this,SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
